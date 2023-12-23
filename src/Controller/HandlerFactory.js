5093e2be22c3fbad4ad11596583fc3bc96db7191
Adenola Omotayo <adenolaomotayo@gmail.com>
2023-12-23 07:49:56 +0100

    Initial commit

--- src/Controller/HandlerFactory.js
+++ src/Controller/HandlerFactory.js
@@ -1,14 +1,3 @@
-const catchAsync = require("./catchAsync");
-// exports.getAllProducts = (Model) =>
-//   catchAsync(async function (req, res, next) {
-//     const doc = Model.find(req.body);
-
-//     res.status(200).json({
-//       status: "Success",
-//       data: { doc },
-//     });
-//   });
-
 exports.getAllProducts("/products", async (req, res) => {
   res.status(200).json({
     status: "success",
@@ -16,14 +5,4 @@ exports.getAllProducts("/products", async (req, res) => {
   });
 });
 
-// exports.getAllProducts = (model) => {
-//   catchAsync(async (req, res, next) => {
-//     let query = model.find(req.body);
-//     const doc = await query;
-//     res.status(200).json({
-//       status: "success",
-//       data: { doc },
-//     });
-//     next();
-//   });
-// };
+
--- src/Page/Search.jsx
+++ src/Page/Search.jsx
@@ -1,14 +1,24 @@
 import { useEffect, useState } from "react";
+// import Products from "../models/ProductModel";
 function Search() {
   const [products, setProducts] = useState([]);
 
-  const { textDescription, title, description, price, shipPrice } = products;
+  // const { textDescription, title, description, price, shipPrice } = Products;
 
   useEffect(function () {
     async function getProductDetails() {
-      const res = await fetch("http://localhost:3000/api/products");
-      const data = await res.json();
-      setProducts(data);
+      try {
+        const res = await fetch("http://localhost:3000/api/products");
+        if (!res.ok)
+          throw new Error(
+            "Something went wrong while loading the products from the database."
+          );
+        const data = await res.json();
+        if (data.Response === "False") throw new Error("Product not found");
+        setProducts(data);
+      } catch (err) {
+        console.log(err);
+      }
     }
     getProductDetails();
 
@@ -17,11 +27,11 @@ function Search() {
 
   return (
     <>
-      <h1>{textDescription}</h1>
-      <h1>{title}</h1>
-      <h1>{console.log(description)}</h1>
-      <h1>{price}</h1>
-      <h1>{shipPrice}</h1>
+      {/* <h1>{textDescription}</h1> */}
+      {/* <h1>{title}</h1> */}
+      <h1>{console.log(products.description)}</h1>
+      {/* <h1>{price}</h1> */}
+      {/* <h1>{shipPrice}</h1> */}
     </>
   );
 }
